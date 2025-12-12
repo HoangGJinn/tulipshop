eead4ca85d1162c0f664526a7f83233adae1c635
Duy3189 <duongminhduy1592005@gmail.com>
2025-12-13 01:24:05 +0700

    Thêm phần checkout, thêm nhiều dịa chỉ đặt hàng trong profile người dùng, thêm một vài chính sách trong chatbot

---
+++ src/main/java/com/tulip/dto/OrderCreationDTO.java
@@ -0,0 +1,10 @@
+package com.tulip.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderCreationDTO {
+    private Long addressId;         // ID địa chỉ người dùng chọn
+    private String paymentMethod;   // "COD", "VNPAY", ...
+    private String note;            // Ghi chú đơn hàng (nếu có)
+}
