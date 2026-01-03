26308b32e5b9ef16e976996e9fdde277b5756045
vinhVVN <vinh.thanhnguyen26@gmail.com>
2026-01-03 20:59:43 +0700

    feat: wishlist alert

--- src/main/java/com/tulip/service/EmailService.java
+++ src/main/java/com/tulip/service/EmailService.java
@@ -1,6 +1,8 @@
 package com.tulip.service;
 
 import com.tulip.entity.Order;
+import com.tulip.entity.User;
+import com.tulip.entity.product.Product;
 
 public interface EmailService {
     void sendOTPToEmail(String toEmail, String otp, String type);
@@ -15,6 +17,14 @@ public interface EmailService {
      * Send rating reminder email after order is delivered
      */
     void sendRatingReminderEmail(Order order);
+    
+    /**
+     * Send wishlist stock alert email
+     * @param user User to notify
+     * @param product Product that changed stock status
+     * @param type Alert type: "LOW_STOCK" or "BACK_IN_STOCK"
+     */
+    void sendWishlistStockAlert(User user, Product product, String type);
     
     /**
      * @deprecated Use sendOrderUpdateEmail instead
